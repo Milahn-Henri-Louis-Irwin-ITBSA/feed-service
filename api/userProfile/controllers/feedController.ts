3d23b1e9c8da9d4c5edde587aebb599143338020
Louis Oosthuizen <ljdragon1997@gmail.com>
2023-10-21 17:13:34 +0200

    feat(feedController.ts):Added Controller
    Added new controllers with changes to body fields expected. Please review and lemme know if yall have any feedback. V1 so to say.

---
+++ api/userProfile/controllers/feedController.ts
@@ -0,0 +1,66 @@
+import {
+  JsonController,
+  Post,
+  BodyParam,
+  HeaderParam,
+} from 'routing-controllers';
+import { LOGGING_INFO } from '../userApiInfo';
+import { Service } from 'typedi';
+import LoggingSvc from '../service/LoggingSvc';
+import { DecodedIdToken } from 'firebase-admin/auth';
+@JsonController(LOGGING_INFO.contextPath + '/logger')
+@Service()
+export class LoggingController {
+  constructor(public _loggingSvc: LoggingSvc) {}
+  @Post('/submitfeed')
+  public async submitLog(
+    @HeaderParam('Authorization') token: string,
+    @BodyParam('message') message: string,
+    @BodyParam('tag') tag: string,
+    @BodyParam('location') location: string
+  ): Promise<any> {
+    try {
+      if (!message) {
+        return Promise.reject({
+          status: 400,
+          message: 'Invalid log',
+        });
+      }
+      // make sure all the body params are present
+      if (!message || !tag || !location || !token) {
+        return Promise.reject({
+          status: 400,
+          message: 'Missing required body params',
+        });
+      }
+      token = token.split(' ')[1];
+
+      const decodedToken: DecodedIdToken | Error =
+        await this._loggingSvc.verifyToken(token);
+
+      if (decodedToken instanceof Error) {
+        return Promise.resolve({
+          status: 401,
+          message: 'Invalid token',
+        });
+      }
+      const resp = await this._loggingSvc.submitLog({
+        message,
+        tag,
+        location,
+        user_uid: decodedToken.uid,
+        response_code: 200,
+      });
+      return Promise.resolve({
+        status: 200,
+        message: 'Feed service updated successfully',
+        data: resp,
+      });
+    } catch (error) {
+      return Promise.reject({
+        status: 500,
+        message: error,
+      });
+    }
+  }
+}
