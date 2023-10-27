dbce3870763ae3da6c268dc36c8723602d3e18ac
Irwin Rudd <irwin.crudd@gmail.com>
2023-10-27 12:19:13 +0200

    Added logger to db
    TODO: Refactor actual log to db. Check logging to audit service.

---
+++ api/logging/loggingApiInfo.ts
@@ -0,0 +1,5 @@
+import { ILoggingInfo } from './model/LoggingInfo';
+
+export const LOGGING_INFO: ILoggingInfo = {
+  contextPath: '/api',
+};
---
+++ types/types.d.ts
@@ -0,0 +1,9 @@
+import type { Timestamp } from 'firebase-admin/firestore';
+import type { UserInfo } from 'firebase-admin/auth';
+export type LoggingData = {
+  created_at: Timestamp;
+  type: string;
+  written_by: string;
+  message: string;
+  user_uid: UserInfo;
+};
